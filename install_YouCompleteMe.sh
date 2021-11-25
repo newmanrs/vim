f7e044aaafbee24cfdf4e813c6a6427672341d29
newmanrs <newmanrs@umich.edu>
2021-11-24 23:28:34 -0800

    use system youcompleteme, source build not worth

--- install_YouCompleteMe.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "See https://github.com/ycm-core/YouCompleteMe.git for other dependencies to install"
-echo "at least: apt install build-essential cmake vim-nox python3-dev"
-echo "and: apt install mono-complete golang nodejs default-jdk npm"
-cd YouCompleteMe
-git submodule update --init --recursive
-python3 install.py --all
-cd ../
